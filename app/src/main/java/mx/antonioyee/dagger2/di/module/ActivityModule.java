57f0d2ab84e8c4944d809455ccef9ec6721b3680
ANTONIO YEE <yee.antonio@gmail.com>
2017-01-02 22:21:16 -0700

    Module

--- app/src/main/java/mx/antonioyee/dagger2/di/module/ActivityModule.java
+++ app/src/main/java/mx/antonioyee/dagger2/di/module/ActivityModule.java
@@ -1,8 +1,34 @@
 package mx.antonioyee.dagger2.di.module;
 
+import android.app.Activity;
+import android.content.Context;
+
+import dagger.Module;
+import dagger.Provides;
+import mx.antonioyee.dagger2.di.ActivityContext;
+
 /**
  * Created by antonioyee on 31/12/16.
  */
 
+@Module
 public class ActivityModule {
+
+    private Activity mActivity;
+
+    public ActivityModule(Activity activity) {
+        mActivity = activity;
+    }
+
+    @Provides
+    @ActivityContext
+    Context provideContext() {
+        return mActivity;
+    }
+
+    @Provides
+    Activity provideActivity() {
+        return mActivity;
+    }
+
 }
--- app/src/main/java/mx/antonioyee/dagger2/di/module/ApplicationModule.java
+++ app/src/main/java/mx/antonioyee/dagger2/di/module/ApplicationModule.java
@@ -1,8 +1,53 @@
 package mx.antonioyee.dagger2.di.module;
 
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import dagger.Module;
+import dagger.Provides;
+import mx.antonioyee.dagger2.di.ApplicationContext;
+import mx.antonioyee.dagger2.di.DatabaseInfo;
+
 /**
  * Created by antonioyee on 31/12/16.
  */
 
+@Module
 public class ApplicationModule {
+
+    private final Application application;
+
+    public ApplicationModule(Application app) {
+        application = app;
+    }
+
+    @Provides
+    @ApplicationContext
+    Context provideContext() {
+        return application;
+    }
+
+    @Provides
+    Application provideApplication() {
+        return application;
+    }
+
+    @Provides
+    @DatabaseInfo
+    String provideDatabaseName() {
+        return "demo-dagger.db";
+    }
+
+    @Provides
+    @DatabaseInfo
+    Integer provideDatabaseVersion() {
+        return 2;
+    }
+
+    @Provides
+    SharedPreferences provideSharedPrefs() {
+        return application.getSharedPreferences("demo-prefs", Context.MODE_PRIVATE);
+    }
+
 }
