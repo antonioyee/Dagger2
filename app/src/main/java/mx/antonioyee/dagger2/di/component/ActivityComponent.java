a7abda69852eb774c4e936fad7db07a3ead7320f
ANTONIO YEE <yee.antonio@gmail.com>
2017-01-02 22:20:40 -0700

    Components

--- app/src/main/java/mx/antonioyee/dagger2/di/component/ActivityComponent.java
+++ app/src/main/java/mx/antonioyee/dagger2/di/component/ActivityComponent.java
@@ -1,8 +1,18 @@
 package mx.antonioyee.dagger2.di.component;
 
+import dagger.Component;
+import mx.antonioyee.dagger2.MainActivity;
+import mx.antonioyee.dagger2.di.PerActivity;
+import mx.antonioyee.dagger2.di.module.ActivityModule;
+
 /**
  * Created by antonioyee on 31/12/16.
  */
 
-public class ActivityComponent {
+@PerActivity
+@Component(dependencies = ApplicationComponent.class, modules = ActivityModule.class)
+public interface ActivityComponent {
+
+    void inject(MainActivity mainActivity);
+
 }
--- app/src/main/java/mx/antonioyee/dagger2/di/component/ApplicationComponent.java
+++ app/src/main/java/mx/antonioyee/dagger2/di/component/ApplicationComponent.java
@@ -1,8 +1,37 @@
 package mx.antonioyee.dagger2.di.component;
 
+import android.app.Application;
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+import mx.antonioyee.dagger2.DemoApplication;
+import mx.antonioyee.dagger2.data.DataManager;
+import mx.antonioyee.dagger2.data.DbHelper;
+import mx.antonioyee.dagger2.data.SharedPrefsHelper;
+import mx.antonioyee.dagger2.di.ApplicationContext;
+import mx.antonioyee.dagger2.di.module.ApplicationModule;
+
 /**
  * Created by antonioyee on 31/12/16.
  */
 
-public class ApplicationComponent {
+@Singleton
+@Component(modules = ApplicationModule.class)
+public interface ApplicationComponent {
+
+    void inject(DemoApplication demoApplication);
+
+    @ApplicationContext
+    Context getContext();
+
+    Application getApplication();
+
+    DataManager getDataManager();
+
+    SharedPrefsHelper getPreferenceHelper();
+
+    DbHelper getDbHelper();
+
 }
